6843b35bf45b8fdf799c9afa61e34e3d65ea1418
yamamoto0315 <48265326+yamamoto0315@users.noreply.github.com>
2021-08-16 21:33:33 +0800

    Add files via upload

---
+++ popdog-hack.js
@@ -0,0 +1,25 @@
+/*
+
+    1.  set username
+    2.  Click F12
+    3.  Go to console
+    4.  Paste the following code and enter
+
+*/
+
+var interval = () =>{
+fetch('/clicked/v2', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({'clicks' : '1000', 'uuid' : localStorage.getItem("uuid"), 'username' : localStorage.getItem("username")})
+      }) 
+      .then(response => response.json())
+      .then(data => {
+        console.log(data['clicks']);
+        document.getElementById('myClicks').innerHTML = data['clicks'].toLocaleString();
+        interval();
+      })
+};
+interval();
